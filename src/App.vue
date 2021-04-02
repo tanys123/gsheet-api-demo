9e0309445f40cc1a7eaa7f63021bec5f226b76af
Terry Tan YongSheng <tanyongsheng0805@gmail.com>
2021-04-02 20:22:34 +0800

    Test out gsheet api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SheetDisplay />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SheetDisplay from './components/SheetDisplay.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SheetDisplay
   }
 }
 </script>
---
+++ src/components/SheetDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    Hello world
+  </div>
+</template>
+
+<script>
+const GSheetReader = require('g-sheets-api');
+
+export default {
+  mounted() {
+    this.load()
+  },
+  methods: {
+    load: function () {
+      const options = {
+        sheetId: '1eabQ7flBgElDFilBO4ErkLPIb7LtokDATLF8EAUR6J8',
+        sheetNumber: 1,
+        returnAllResults: true
+      }
+
+      GSheetReader(
+        options,
+        results => {
+          console.log(results)
+        },
+        error => {
+          console.log(error)
+        }
+      );
+    }
+  }
+}
+</script>
